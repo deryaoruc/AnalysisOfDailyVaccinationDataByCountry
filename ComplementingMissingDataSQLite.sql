965ea5112ff4d4de94171e75a021c81ab9af0e5a
Derya Oruç <deryaoruc00@gmail.com>
2022-10-29 02:45:08 +0300

    -

---
+++ ComplementingMissingDataSQLite.sql
@@ -0,0 +1,30 @@
+-- TABLE
+CREATE TABLE 'country_vaccination_stats' ('country' TEXT,'date' TEXT,'daily_vaccinations' INTEGER,'vaccines' TEXT);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE grouppedMedian(
+  country TEXT,
+  daily_vaccinations_m
+);
+CREATE TABLE Merged(
+  country TEXT,
+  date TEXT,
+  daily_vaccinations INT,
+  vaccines TEXT,
+  daily_vaccinations_m,
+  x TEXT
+);
+CREATE TABLE NEW(
+  country TEXT,
+  date TEXT,
+  daily_vaccinations INT,
+  vaccines TEXT,
+  daily_vaccinations_m,
+  x TEXT
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
