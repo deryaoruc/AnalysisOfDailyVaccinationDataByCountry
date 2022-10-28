42702784f338353fcd4428b0c1bf9d916f3a4a9b
Derya Oruç <deryaoruc00@gmail.com>
2022-10-29 01:09:32 +0300

    New files added, and old files renamed

--- TopThreeCountyPandas.py
+++ TopThreeCountyPandas.py
@@ -9,6 +9,7 @@ dfGroupMin = dfGroup.min().reset_index()
 dfGroupMinFull = dfGroupMin.fillna(0)
 
 df = df.fillna(dfGroupMinFull)
+print(df)
 
 
 # Top 3 countries with the highest median daily vaccination numbers
@@ -16,10 +17,11 @@ df = df.fillna(dfGroupMinFull)
 groupped = df.groupby("country")
 grouppedMedian = groupped.median().reset_index()
 grouppedMedianTopThree = grouppedMedian.head(3)
+print(grouppedMedian)
 
 
 # Number of total vaccinations done on 1/6/2021 (MM/DD/YYYY)
 
-x = df.groupby("date").sum().reset_index()
-xSorted = x.sort_values('date',ascending=True)
-print(x)
+# x = df.groupby("date").sum().reset_index()
+# xSorted = x.sort_values('date',ascending=True)
+# print(x)
