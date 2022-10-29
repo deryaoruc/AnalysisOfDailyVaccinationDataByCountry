2bedd59de0a479cf01042cf98f11d871510dd13b
Derya Oruç <deryaoruc00@gmail.com>
2022-10-29 15:11:11 +0300

    sql file modified

--- ComplementingMissingDataSQLite.sql
+++ ComplementingMissingDataSQLite.sql
@@ -1,30 +1,18 @@
--- TABLE
-CREATE TABLE 'country_vaccination_stats' ('country' TEXT,'date' TEXT,'daily_vaccinations' INTEGER,'vaccines' TEXT);
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
-CREATE TABLE grouppedMedian(
-  country TEXT,
-  daily_vaccinations_m
-);
-CREATE TABLE Merged(
-  country TEXT,
-  date TEXT,
-  daily_vaccinations INT,
-  vaccines TEXT,
-  daily_vaccinations_m,
-  x TEXT
-);
-CREATE TABLE NEW(
-  country TEXT,
-  date TEXT,
-  daily_vaccinations INT,
-  vaccines TEXT,
-  daily_vaccinations_m,
-  x TEXT
-);
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+CREATE TEMP TABLE grouppedMedian AS 
+SELECT country, date, 
+COALESCE(MEDIAN(daily_vaccinations),0) AS daily_vaccinations_median, 
+vaccines
+FROM country_vaccination_stats c
+GROUP by country
+
+CREATE TEMP TABLE merged AS 
+SELECT c.country, c.date, c.daily_vaccinations, 
+c.vaccines, g.country,g.daily_vaccinations_median
+FROM country_vaccination_stats c
+INNER JOIN grouppedMedian g ON c.country=g.country
+
+CREATE TABLE new_country_vaccination_stats AS 
+SELECT country,date,
+COALESCE(daily_vaccinations,daily_vaccinations_median) AS daily_vaccinations,
+vaccines
+FROM merged
